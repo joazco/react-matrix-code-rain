6d2d334c7e14282e1afe837635c9983f7208a1c5
Jordan AZOULAY <jazoulay@joazco.com>
2023-07-13 13:01:00 +0200

    append example

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import MatrixCodeRainComponent from 'react-matrix-code-rain';
+
+function App() {
+  return (
+    <div className="App">
+      <MatrixCodeRainComponent />
+    </div>
+  );
+}
+
+export default App;
